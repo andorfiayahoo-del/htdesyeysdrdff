c6ef742328e63bd068da503660b78db3ef53b478
Ander <AndersWR@hotmail.no>
2025-08-20 09:53:33 +0200

    inbox: apply patch_0421_fix_0700_full.patch

--- Assets/Editor/InboxCompileSignal_0700.cs
+++ Assets/Editor/InboxCompileSignal_0700.cs
@@ -1,1 +1,23 @@
 #if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class InboxCompileSignal_0700
+{
+    static InboxCompileSignal_0700()
+    {
+        try
+        {
+            var stamp = System.IO.Path.Combine(Application.dataPath, "InboxPatches", "CompileDone.stamp");
+            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(stamp));
+            System.IO.File.WriteAllText(stamp, System.DateTime.UtcNow.ToString("o"));
+            Debug.Log("[Inbox] Wrote CompileDone.stamp");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogWarning("[Inbox] Failed to write CompileDone.stamp: " + ex.Message);
+        }
+    }
+}
+#endif
