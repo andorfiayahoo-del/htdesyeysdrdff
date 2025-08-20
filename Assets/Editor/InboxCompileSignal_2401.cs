357b7a7e018d1de9a399b75f6443a0b26ad9d1e7
Ander <AndersWR@hotmail.no>
2025-08-20 12:34:42 +0200

    inbox: apply patch_2401.patch

---
+++ Assets/Editor/InboxCompileSignal_2401.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class InboxCompileSignal_2401
+{
+    static InboxCompileSignal_2401()
+    {
+        try
+        {
+            var stamp = System.IO.Path.Combine(Application.dataPath, "InboxPatches", "CompileDone.stamp");
+            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(stamp));
+            System.IO.File.WriteAllText(stamp, System.DateTime.UtcNow.ToString("o"));
+            Debug.Log("[Inbox] 2401 wrote CompileDone.stamp");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogWarning("[Inbox] 2401 failed to write CompileDone.stamp: " + ex.Message);
+        }
+    }
+}
+#endif
+
