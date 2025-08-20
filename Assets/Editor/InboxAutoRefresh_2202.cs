819ecb177cb0e90bb2eec1018290ee0d6033642f
Ander <AndersWR@hotmail.no>
2025-08-20 11:28:04 +0200

    inbox: reconcile patch_2202.patch

---
+++ Assets/Editor/InboxAutoRefresh_2202.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+[InitializeOnLoad]
+public static class InboxAutoRefresh_2202
+{
+    static InboxAutoRefresh_2202()
+    {
+        EditorApplication.update += DoOnce;
+    }
+    private static void DoOnce()
+    {
+        EditorApplication.update -= DoOnce;
+        AssetDatabase.Refresh(ImportAssetOptions.Default);
+        Debug.Log("[Inbox] Auto-refresh 2202 complete.");
+    }
+}
+#endif
+
