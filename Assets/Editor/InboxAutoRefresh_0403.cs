cd7d6b65f407f6c6c71bdce8ac895e6a933f632f
Ander <AndersWR@hotmail.no>
2025-08-20 09:53:35 +0200

    inbox: apply patch_0422_fix_0403_full.patch

--- Assets/Editor/InboxAutoRefresh_0403.cs
+++ Assets/Editor/InboxAutoRefresh_0403.cs
@@ -1,1 +1,20 @@
 #if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class InboxAutoRefresh_0403
+{
+    static InboxAutoRefresh_0403()
+    {
+        EditorApplication.update += DoOnce;
+    }
+
+    private static void DoOnce()
+    {
+        EditorApplication.update -= DoOnce;
+        AssetDatabase.Refresh(ImportAssetOptions.Default);
+        Debug.Log("[Inbox] Auto-refresh 0403 complete.");
+    }
+}
+#endif
