58c3d64aa32fe81fa1f81e910df973ea5249954b
Ander <AndersWR@hotmail.no>
2025-08-20 12:35:31 +0200

    inbox: apply patch_2402.patch

---
+++ Assets/Editor/InboxAutoRefresh_2402.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class InboxAutoRefresh_2402
+{
+    static InboxAutoRefresh_2402()
+    {
+        EditorApplication.update += DoOnce;
+    }
+
+    private static void DoOnce()
+    {
+        EditorApplication.update -= DoOnce;
+        AssetDatabase.Refresh(ImportAssetOptions.Default);
+        Debug.Log("[Inbox] Auto-refresh 2402 complete.");
+    }
+}
+#endif
+
