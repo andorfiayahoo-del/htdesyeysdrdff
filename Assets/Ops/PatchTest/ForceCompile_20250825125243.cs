e43be9722c9468ca114746cfdaa5d39d53cfba29
Ander <AndersWR@hotmail.no>
2025-08-25 12:52:52 +0200

    sync: auto (manual) 2025-08-25T10:52:52.753Z

---
+++ Assets/Ops/PatchTest/ForceCompile_20250825125243.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Ops.PatchTest {
+  public sealed class ForceCompile_20250825125243 : MonoBehaviour {
+    void Awake() { Debug.Log("[ForceCompile] injected script loaded"); }
+  }
+}
